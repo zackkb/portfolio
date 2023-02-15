87611669bb8499721421bb428d31db07703f4269
zackkb <98679473+zackkb@users.noreply.github.com>
2023-02-15 08:24:19 -0600

    Adding content to About

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 import "./About.scss";
 
+const abouts = [
+  {
+    title: "Web Development",
+    description: "I am a good web developer.",
+    imgURL: "",
+  },
+  {
+    title: "Web Design",
+    description: "I am a good web developer.",
+    imgURL: "",
+  },
+  { title: "UI/UX", description: "I am a good web developer.", imgURL: "" },
+  {
+    title: " Web Animations",
+    description: "I am a good web developer.",
+    imgURL: "",
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgURL} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
